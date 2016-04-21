e76970fbbdbd6be4d6e370b42b30ea973b8b6ca7
Antonio Paulo <apaulo2007@gmail.com>
2016-04-21 19:29:52 -0300

    Comitar Algoritmo_06

---
+++ Volume_1/Algoritmo_06/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_06
+ * 
+ *  Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida 
+ *  ela possui. Considere sempre anos completos, e que um ano possui 365 dias. Ex: uma pessoa 
+ *  com 19 anos possui 6935 dias de vida; veja um exemplo de saída: MARIA, VOCÊ JÁ VIVEU 6935 
+ *  Dias
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializando e declarando as variáveis
+            string nome = "";
+            int idade = 0;
+            const int ANO = 365;
+            int resultado = 0;
+
+            Console.Write("Entre com seu nome : ");
+            nome = Console.ReadLine();
+
+            Console.Write("Entre com sua idade : ");
+            idade = int.Parse(Console.ReadLine());
+
+            // processamento da conversão da idade em anos para idade em dias
+            resultado = idade * ANO;
+
+            Console.Write(nome + " , Você ja viveu " + resultado + " dias");
+
+            Console.ReadKey();  // espera entrada de teclado para encerrar o programa 
+        }
+    }
+}
