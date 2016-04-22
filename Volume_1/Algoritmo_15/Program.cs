53c84f5d1de00d0c5eed3ba5006f328ebeafd7c4
Antonio Paulo <apaulo2007@gmail.com>
2016-04-21 22:25:19 -0300

    comitar Algoritmo_15

---
+++ Volume_1/Algoritmo_15/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_15
+ * 
+ * . Calcule o volume de uma caixa d'água cilíndrica. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializa variáveis e constante
+            const double PI = 3.14d;
+            double raio = 0;
+            double altura = 0;
+            double area = 0;
+
+            // exibe no console
+            Console.Write("Entre com o raio da caixa d' agua cilindrica : ");
+            raio = double.Parse(Console.ReadLine());   // lê raio  
+
+            // exibe no console
+            Console.Write("Entre com a altura da caixa d'agua cilindrica : ");
+            altura = double.Parse(Console.ReadLine()); // lê altura
+
+            // processamento
+            area = (PI * (raio * raio)) * altura;
+
+            // saída ou solução
+            Console.Write("Area da caixa d'agua cilindrica é " + area);
+            
+            // espera entrada do teclado para encerrar o programa
+            Console.ReadKey();
+
+
+        }
+    }
+}
