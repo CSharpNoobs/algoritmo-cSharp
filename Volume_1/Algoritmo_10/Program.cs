415f6853b07f847af54d3156acfffc1b3f116690
Antonio Paulo <apaulo2007@gmail.com>
2016-04-21 21:20:12 -0300

    comitar Algoritmo_10

---
+++ Volume_1/Algoritmo_10/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_10
+ * 
+ * Faça um algoritmo que leia um número e imprima seu dobro. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializa e declara variáveis
+            int numero = 0;
+            int dobro = 0;
+
+            // exibe no console
+            Console.Write("Entre com um numero : ");
+            numero = int.Parse(Console.ReadLine());   // lê o numero
+
+            // processamento 
+            dobro = numero * 2;
+
+            // saida ou solução do programa 
+            Console.Write("O dobro do numero " + numero + " é : " + dobro);
+
+            Console.ReadKey();   // espera entrada de teclado para encerrar o programa 
+        }
+    }
+}
