d90815515cd35d2b5543bf638157cfe1fa5e73d1
Antonio Paulo <apaulo2007@gmail.com>
2016-04-21 21:32:54 -0300

    comitar Algoritmo_12

---
+++ Volume_1/Algoritmo_12/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_11
+ * 
+ * Faça um algoritmo que calcule a area de um quadrado lendo o lado. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializa e declara as variáveis
+            double lado = 0;
+            double area = 0;
+
+            // exibe no console
+            Console.Write("Entre com o valor do lado do quadrado : ");
+            lado = double.Parse(Console.ReadLine());   // lê o lado
+
+            // processamento
+            area = lado * lado;
+
+            // saida ou resultado do programa
+            Console.Write("Área do quadrado é : " + area + " metros quadrados");
+
+            // espera entrada do teclado para encerrar o programa
+            Console.ReadKey();
+
+        }
+    }
+}
