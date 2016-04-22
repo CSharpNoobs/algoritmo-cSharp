f02c6ecdec77d478cfb05228ae390f28dd169294
Antonio Paulo <apaulo2007@gmail.com>
2016-04-22 20:12:17 -0300

    Algoritmo_21

---
+++ Volume_1/Algoritmo_21/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_21
+ * 
+ * Faça um algoritmo para ler um preço de um produto em reais e a taxa de conversão do dólar e informar
+ * o preço do produto em dólares.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializa e declara variáveis 
+            double precoReal = 0;
+            double precoDolar = 3.74d;
+            double conversao = 0;
+
+            // exibe no console
+            Console.Write("Entre com preço em Real : ");
+            precoReal = double.Parse(Console.ReadLine());   // lê o preço em real 
+
+            // processamento para conversão em dólar
+            conversao = precoReal / precoDolar;
+
+            // saída ou solução 
+            Console.Write("Preço convertido em dolar : $" + conversao + " dolares");
+
+            // espera entrada do teclado para encerrar o programa
+            Console.ReadKey();
+        }
+    }
+}
