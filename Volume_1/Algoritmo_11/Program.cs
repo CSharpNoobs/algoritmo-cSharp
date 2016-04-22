01657b5abfebd010d5da928d113ef0623ab7e715
Antonio Paulo <apaulo2007@gmail.com>
2016-04-21 21:27:29 -0300

    comitar Algoritmo_11

---
+++ Volume_1/Algoritmo_11/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_11
+ * 
+ * Faça um algoritmo que leia um número e imprima seu quadrado. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializa e declara variáveis 
+            double numero = 0;
+            double quadrado = 0;
+
+            // exibe no console
+            Console.Write("Entre com um número : ");
+            numero = double.Parse(Console.ReadLine());
+
+            // processamento
+            quadrado = numero * numero;
+
+            // saída ou solucção do programa
+            Console.Write("O quadrado do numero " + numero + " é : " + quadrado);
+
+            // espera uma entrada do teclado para fechar o programa
+            Console.ReadKey();
+        }
+    }
+}
