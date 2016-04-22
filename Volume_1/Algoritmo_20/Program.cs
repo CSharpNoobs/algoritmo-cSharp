395cf42e461690388e1a1c2be09ea30c4ccc417f
Antonio Paulo <apaulo2007@gmail.com>
2016-04-22 13:21:58 -0300

    comitar Algoritmo_20

---
+++ Volume_1/Algoritmo_20/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_20
+ * 
+ * Faça um algoritmo para ler uma temperatura dada na escala Fahrenheit e exibir o equivalente em Celsius.
+ * C = 5/9(F-32)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicializa e declara as variáveis
+            double f;
+            double c;
+
+            // exibe no console
+            Console.Write("Entre com a temperatura na escala Fahrenheit : ");
+            f = double.Parse(Console.ReadLine()); // le a temperatura fahrenheit
+
+            // processamento
+            c = (double)5/9 * (f - 32);
+
+            // saida ou solução
+            Console.Write("A temperatura em Celsius é igual a : " + c);
+
+            // espera entrada do teclado para encerrar a aplicação
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
