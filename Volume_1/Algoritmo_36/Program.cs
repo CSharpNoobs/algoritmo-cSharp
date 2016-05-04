cb52f37222c7f8d114962f37aaf2515a0f1b0a81
Antonio Paulo <apaulo2007@gmail.com>
2016-05-04 16:22:49 -0300

    Corrigir

--- Volume_1/Algoritmo_36/Program.cs
+++ Volume_1/Algoritmo_36/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * Author  : Antônio Paulo Neto
  * File    : Program.cs
- * Project : Algoritmo_06
+ * Project : Algoritmo_36
  * 
  * Faça um algoritmo que receba um valor que foi depositado e exiba o
  * valor com rendimento após um mês. Considere fixo o juro da
