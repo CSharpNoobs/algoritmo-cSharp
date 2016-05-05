55cd533121ce233f1be3c7bcdf634765456f7f9c
Antonio Paulo <apaulo2007@gmail.com>
2016-05-05 14:43:37 -0300

    Comitar  Algoritmo_38 | Algoritmo_39

---
+++ Volume_1/Algoritmo_39/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_39
+ * 
+ * Entrar com quatro números e imprimir a média ponderada, sabendo-se que os pesos são respectivamente: 1, 2, 3 e 4. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            double d = 0;
+            double mp = 0;
+
+            // exibi no console
+            Console.Write("Entre com o primeiro número : ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o segundo número : ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o terceiro número : ");
+            c = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o quarto número : ");
+            d = double.Parse(Console.ReadLine());
+
+            // processamento
+            mp = (a * 1 + b * 2 + c * 3 + d * 4) / 10;
+ 
+            // saida
+            Console.Write("\nMedia ponderada : " + mp + "\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Volume_1/Algoritmo_40/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_39
+ * 
+ * Antes de o racionamento de energia ser decretado, quase ninguém falava em quilowatts; mas, agora, todos incorporaram essa palavra em seu vocabulário. Sabendo-se que 100 quilowatts de energia custa um sétimo do salário mínimo, fazer um algoritmo que receba o valor do salário mínimo e a quantidade de quilowatts gasta por uma residência e calcule. Imprima 
+ * o valor em reais de cada quilowatt 
+ * o valor em reais a ser pago 
+ * o novo valor a ser pago por essa residência com um desconto de 10%. 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sm = 0;
+            double qtdade = 0;
+            double preco = 0;
+            double vp = 0;
+            double vd = 0;
+
+            Console.Write("Entre com o salário mínimo : ");
+            sm = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com a quantidade em quilowatt : ");
+            qtdade = double.Parse(Console.ReadLine());
+
+            // processamento
+            preco = sm / 700; // divide por 7 para achar o preço de 100 kw e por 100 para achar de 1kw
+            vp = preco * qtdade;
+            vd = vp * 0.9;
+
+            Console.Write("Preço do quilowatt : " + preco + "\n");
+            Console.Write("Valor a ser pago   : " + vp +    "\n")
+        }
+    }
+}
