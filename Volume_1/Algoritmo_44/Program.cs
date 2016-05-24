7acfa0c08e0e3e4ef1294c24d0a96d75b58b10a1
Antonio Paulo <apaulo2007@gmail.com>
2016-05-23 22:01:02 -0300

    comitar Algoritmo_44

---
+++ Volume_1/Algoritmo_44/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author  : Antônio Paulo Neto
+ * File    : Program.cs
+ * Project : Algoritmo_44
+ * 
+ * Algoritmo que converta km/h para mph. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double mph = 1.6093d;
+            double kmh = 0;
+            double converter = 0;
+
+            Console.Write("Entre a velocidade em km/h : ");
+            kmh = double.Parse(Console.ReadLine());
+
+            converter = kmh * mph;
+
+            Console.Write(kmh + " Km/h convertido em Mhp = "+ converter + " mph");
+
+            Console.ReadKey();
+        }
+    }
+}
